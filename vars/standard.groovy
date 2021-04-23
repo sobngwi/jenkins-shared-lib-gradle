e81a60c8487e88f174ecc1c591244f1618daf2aa
Alain Narcisse SOBNGWI <sobngwi@gmail.com>
2021-04-23 08:59:06 +0200

    The standard library using vars.

--- vars/standard.groovy
+++ vars/standard.groovy
@@ -8,6 +8,7 @@ def call(String goToolName = 'go-1.12', String golangCiVersion = 'v1.18.0') {
         stages {
             stage('Gradle version with global var') {
                 steps {
+                    sh 'chmod +x gradlew'
                     gradlew '-v'
                 }
             }
