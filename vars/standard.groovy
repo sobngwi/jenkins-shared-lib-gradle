1a17d0b68755af5329c9c40fd963f9fdbb6e09ef
Alain Narcisse SOBNGWI <sobngwi@gmail.com>
2021-04-23 08:46:14 +0200

    The standard library using vars.

---
+++ vars/standard.groovy
@@ -0,0 +1,16 @@
+def call(String goToolName = 'go-1.12', String golangCiVersion = 'v1.18.0') {
+    pipeline {
+        agent any
+
+        triggers {
+            pollSCM('*/5 * * * *')
+        }
+        stages {
+            stage('Gradle version with global var') {
+                steps {
+                    gradlew '-v'
+                }
+            }
+        }
+    }
+}
