2821e6cc3b300dad2a153e07d6005fe2912c294d
Alain Narcisse SOBNGWI <sobngwi@gmail.com>
2021-04-23 09:09:36 +0200

    The standard library using vars.

--- vars/standard.groovy
+++ vars/standard.groovy
@@ -1,15 +1,22 @@
+import com.bmuschko.jenkins.Gradle
+
 def call(String goToolName = 'go-1.12', String golangCiVersion = 'v1.18.0') {
+    @Library('gradle-shared-lib@2.0') import com.bmuschko.jenkins.Gradle
+    gradle = new Gradle(this)
     pipeline {
         agent any
 
         triggers {
             pollSCM('*/5 * * * *')
         }
+
         stages {
             stage('Gradle version with global var') {
                 steps {
                     sh 'chmod +x gradlew'
-                    gradlew '-v'
+                    script {
+                        gradle.wrapper '-v'
+                    }
                 }
             }
         }
